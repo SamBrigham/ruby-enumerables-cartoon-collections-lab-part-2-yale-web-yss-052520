7ae201c26963a9ce04b0579a3a410c51d4fa945e
Samuel Brigha <sam.brigham@yale.edu>
2020-05-15 16:50:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
     "#{element.capitalize}!"
   end
-  
+  p planeteer_calls
 end
 
 def long_planeteer_calls(planeteer_calls)
