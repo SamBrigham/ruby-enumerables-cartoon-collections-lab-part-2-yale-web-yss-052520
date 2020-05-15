218111e2fc68c810caaa2120eb9542a427eb5324
Samuel Brigha <sam.brigham@yale.edu>
2020-05-15 17:50:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.include? {valid_calls}
- return "#{element}"
- end
+ return "#{}"
  end
 
 
